e2cb01d0ab7fdc804cb12b6d368e055cf1d04e5b
Chris Reams <chrisreams91@gmail.com>
2023-05-16 18:34:15 -0500

    variations

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,80 @@
 "use client";
-import * as React from "react";
+import React, { useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Radio, RadioGroup } from "@chakra-ui/react";
-import { Numbers } from "./types";
+import { Radio, RadioGroup, Stack, Heading } from "@chakra-ui/react";
+import {
+  NUMBERS_ENUM,
+  NUMBERS_AS_CONST,
+  NUMBERS_AS_CONST_VALUES,
+  NUMBERS_ENUMIFY,
+} from "./types";
 
 export default function Home() {
-  const [value, setValue] = React.useState<Numbers>(Numbers.ONE);
+  const [valueEnum, setValueEnum] = useState<NUMBERS_ENUM>(NUMBERS_ENUM.ONE);
 
-  console.log(value);
+  const [valueAsConst, setValueAsConst] = useState<NUMBERS_AS_CONST_VALUES>(
+    NUMBERS_ENUM.ONE
+  );
+
+  const [valueEnumify, setValueEnumify] = useState<NUMBERS_ENUMIFY>(
+    NUMBERS_ENUMIFY.ONE
+  );
+
   return (
     <ChakraProvider>
-      <RadioGroup
-        onChange={(nextValue) => setValue(nextValue as Numbers)}
-        value={value}
-      >
-        <Radio value={Numbers.ONE}>First</Radio>
-        <Radio value={Numbers.TWO}>Second</Radio>
-        <Radio value={Numbers.THREE}>Third</Radio>
-      </RadioGroup>
+      <Stack spacing={5} padding={10}>
+        <Heading>Basic enum</Heading>
+        <RadioGroup
+          onChange={(nextValue) => setValueEnum(nextValue as NUMBERS_ENUM)}
+          value={valueEnum}
+        >
+          <Radio padding={5} value={NUMBERS_ENUM.ONE}>
+            ONE
+          </Radio>
+          <Radio padding={5} value={NUMBERS_ENUM.TWO}>
+            TWO
+          </Radio>
+          <Radio padding={5} value={NUMBERS_ENUM.THREE}>
+            THREE
+          </Radio>
+        </RadioGroup>
+      </Stack>
+
+      <Stack spacing={5} padding={10}>
+        <Heading>as const enum</Heading>
+        <RadioGroup
+          onChange={(nextValue) =>
+            setValueAsConst(nextValue as NUMBERS_AS_CONST_VALUES)
+          }
+          value={valueAsConst}
+        >
+          <Radio padding={5} value={NUMBERS_AS_CONST.ONE}>
+            ONE
+          </Radio>
+          <Radio padding={5} value={NUMBERS_AS_CONST.TWO}>
+            TWO
+          </Radio>
+          <Radio padding={5} value={NUMBERS_AS_CONST.THREE}>
+            THREE
+          </Radio>
+        </RadioGroup>
+      </Stack>
+
+      <Stack spacing={5} padding={10}>
+        <Heading>Enumify</Heading>
+        <RadioGroup
+          onChange={(nextValue) =>
+            setValueEnumify(NUMBERS_ENUMIFY.enumValueOf(nextValue)!)
+          }
+          value={valueEnumify.enumKey}
+        >
+          {[...NUMBERS_ENUMIFY].map((number) => (
+            <Radio key={number.enumKey} value={number.enumKey} padding={5}>
+              {number.enumKey}
+            </Radio>
+          ))}
+        </RadioGroup>
+      </Stack>
     </ChakraProvider>
   );
 }
--- app/types.ts
+++ app/types.ts
@@ -1,5 +1,24 @@
-export enum Numbers {
+import { Enumify } from "enumify";
+
+export enum NUMBERS_ENUM {
   ONE = "ONE",
   TWO = "TWO",
   THREE = "THREE",
+}
+
+export const NUMBERS_AS_CONST = {
+  ONE: "ONE",
+  TWO: "TWO",
+  THREE: "THREE",
+} as const satisfies Record<string, string>;
+
+export type NUMBERS_AS_CONST_VALUES = Values<typeof NUMBERS_AS_CONST>;
+
+type Values<T> = T[keyof T];
+
+export class NUMBERS_ENUMIFY extends Enumify {
+  static ONE = new NUMBERS_ENUMIFY();
+  static TWO = new NUMBERS_ENUMIFY();
+  static THREE = new NUMBERS_ENUMIFY();
+  static _ = NUMBERS_ENUMIFY.closeEnum();
 }
