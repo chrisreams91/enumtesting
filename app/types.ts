8fabc7d967d6a216884eed9bdf5ffc9aecd1557d
Miles <miles@karmacheck.com>
2023-05-16 17:28:04 -0500

    a

--- app/types.ts
+++ app/types.ts
@@ -1,5 +1,9 @@
-export enum Numbers {
-  ONE = "ONE",
-  TWO = "TWO",
-  THREE = "THREE",
-}
+export const NUMBERS = {
+  ONE: "ONE",
+  TWO: "TWO",
+  THREE: "THREE",
+} as const satisfies Record<string, string>;
+
+export type Numbers = Values<typeof NUMBERS>;
+
+type Values<T> = T[keyof T];
